5553d593306eb2e87ae973aa5272b95e7ab70b2d
mishaelnuh <mishael.nuh@arup.com>
2019-09-02 17:33:57 -0400

    Add spread operator transpiler.

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -22,7 +22,10 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: [
+      'babel-polyfill',
+      './src/main.js'
+    ]
   },
   output: {
     path: config.build.assetsRoot,
@@ -49,7 +52,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+        include: [resolve('SpeckleUiApp/src'), resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
